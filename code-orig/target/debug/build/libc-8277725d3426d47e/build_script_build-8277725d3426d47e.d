13164bd401f8ca40b7f6f65fc2a3e25b57fefc33
Aaryan Chauhan <100084094+ASC-073@users.noreply.github.com>
2023-11-23 21:23:45 +0530

    Delete code-orig/target/debug/build directory

--- code-orig/target/debug/build/libc-8277725d3426d47e/build_script_build-8277725d3426d47e.d
+++
@@ -1,5 +0,0 @@
-/Users/tanay/Rust/ray-tracing/target/debug/build/libc-8277725d3426d47e/build_script_build-8277725d3426d47e: /Users/tanay/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.150/build.rs
-
-/Users/tanay/Rust/ray-tracing/target/debug/build/libc-8277725d3426d47e/build_script_build-8277725d3426d47e.d: /Users/tanay/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.150/build.rs
-
-/Users/tanay/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.150/build.rs:
